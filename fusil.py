62d6d16f7bead1ad2c1e38863bfcf408a2d96285
--help <ziza595@gmail.com>
2021-06-18 12:00:55 +0000

    Modification du nom robo.py en robot.py et update du module fusil

--- fusil.py
+++ fusil.py
@@ -1,59 +1,181 @@
 import turtle
-import dessinMSDA as d
 
-# triangle du sommet
-turtle.penup()
-turtle.goto(0, 50)
-turtle.pendown()
-d.triangle(100)
 
-# carrée intérieur
+turtle.goto(-50, 0)
+turtle.pensize(2)
+
+# triangle
+for i in range(3):
+    turtle.forward(100)
+    turtle.left(120)
+pos1 = turtle.position()
+# carré
 turtle.penup()
-turtle.forward(25)
+turtle.goto(-25, 0)
 turtle.pendown()
+for i in range(4):
+    turtle.forward(48)
+    turtle.left(90)
+
+def trapeze(a, b):
+    turtle.forward(a)
+    turtle.left(120)
+    turtle.forward(b)
+    turtle.left(60)
+    turtle.forward(b)
+    turtle.left(60)
+    turtle.forward(b)
+
+# trapèze
+turtle.penup()
+turtle.setposition(-100.00, -87.00)
+turtle.pendown()
+trapeze(200, 100)
+# rectangle
+turtle.penup()
+turtle.setposition(pos1)
+turtle.left(30)
+turtle.pendown()
+turtle.forward(88)
+turtle.penup()
+turtle.setposition(50.00,0.00)
+turtle.pendown()
+turtle.forward(88)
+
+# trapèze
+turtle.penup()
+turtle.setposition(-115.00, -114.00)
 turtle.left(90)
-d.carre(47)
-
-# trapèze contenant un carré
-turtle.penup()
-turtle.goto(39, -37)
-turtle.left(270)
 turtle.pendown()
-d.trapeze(100, 76, 20)
-
-# rectangle interieur
-turtle.penup()
-turtle.forward(25)
-turtle.left(90)
-turtle.pendown()
-d.rectangle(87, 76)
-
-# grand trapèze
-turtle.penup()
-turtle.goto(-50, -63)
-turtle.left(-90)
-turtle.pendown()
-turtle.forward(204) #
+turtle.forward(230)
 turtle.left(120)
-turtle.forward(30) #
+turtle.forward(30)
 turtle.left(60)
+pos2 = turtle.position()
 turtle.forward(200)
 turtle.left(60)
+pos3 = turtle.position()
 turtle.forward(30)
-
-# barres verticales
+# rectangle
 turtle.penup()
-turtle.left(120)
-turtle.forward(15)
+turtle.goto(pos3)
+turtle.left(30)
 turtle.pendown()
+turtle.forward(26)
+pos4 = turtle.position()
+turtle.penup()
+turtle.goto(pos2)
+turtle.pendown()
+turtle.forward(26)
+pos5 = turtle.position()
+
+# rectangle
+turtle.penup()
+turtle.setposition(-115.00, -147.00)
 turtle.left(90)
-turtle.forward(30)
-turtle.penup()
-turtle.right(90)
-turtle.forward(144)
-turtle.right(90)
 turtle.pendown()
-turtle.forward(30)
+turtle.forward(230)
+turtle.left(90)
+turtle.forward(33)
+turtle.left(90)
+turtle.forward(230)
+turtle.left(90)
+turtle.forward(33)
+# semi-rectangle
+turtle.penup()
+turtle.goto(pos4)
+turtle.pendown()
+turtle.forward(33)
+pos6 = turtle.position()
+turtle.penup()
+turtle.goto(pos5)
+turtle.pendown()
+turtle.forward(33)
+pos7 = turtle.position()
 
-# fermer la boucle
+# triangle quelconque
+turtle.penup()
+turtle.setposition(pos6)
+turtle.right(8)
+turtle.pendown()
+turtle.forward(50)
+turtle.right(162)
+turtle.forward(50)
+
+# triangle quelconque
+turtle.penup()
+turtle.setposition(pos7)
+turtle.left(170)
+turtle.left(8)
+turtle.pendown()
+turtle.forward(50)
+turtle.left(162)
+turtle.forward(50)
+
+# rectangle
+turtle.penup()
+turtle.setposition(-20.00,-147.02)
+turtle.left(190)
+turtle.pendown()
+for i in range(2):
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(40)
+    turtle.left(90)
+
+# cercles
+turtle.penup()
+turtle.setposition(-80.00,-170.02)
+turtle.pendown()
+turtle.circle(18)
+
+turtle.penup()
+turtle.setposition(45.00,-170.02)
+turtle.pendown()
+turtle.circle(18)
+
+# rectangle
+turtle.penup()
+turtle.setposition(-50.00,-197.02)
+turtle.pendown()
+for i in range(2):
+    turtle.forward(40)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+
+# carré
+turtle.penup()
+turtle.setposition(-15.00,-237.02)
+turtle.pendown()
+for i in range(4):
+    turtle.forward(30)
+    turtle.left(90)
+
+# cercles
+turtle.penup()
+turtle.setposition(-30.00,-255.02)
+turtle.left(90)
+turtle.pendown()
+turtle.circle(5)
+
+turtle.penup()
+turtle.setposition(30.00,-255.02)
+turtle.pendown()
+turtle.circle(5)
+
+# trapeze
+turtle.penup()
+turtle.setposition(-75.00,-310.02)
+turtle.pendown()
+turtle.forward(144)
+turtle.left(142)
+turtle.forward(78)
+turtle.left(40)
+turtle.forward(30)
+turtle.left(30)
+turtle.forward(77)
+
+
+
 turtle.done()
